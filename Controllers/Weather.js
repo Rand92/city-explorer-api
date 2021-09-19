e668eb2061b0b853aa9d09785c03f197506282ea
Rand92 <s.rund@yahoo.com>
2021-09-19 16:06:59 +0300

    recent1

--- Controllers/Weather.js
+++ Controllers/Weather.js
@@ -11,12 +11,14 @@ let handleWeather= async (req,res)=>{
     let lat=req.query.lat;
     let lon=req.query.lon;
     let url=`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&key=${process.env.WEATHERBIT_API_KEY}&lon=${lon}`;
-    let axiosResponse= await axios.get(url);
-    let weatherData=axiosResponse.data;
+   axios.get(url).then(result=>{
+    let weatherData=result.data;
     let cleanedData=weatherData.data.map(item=>{
         return new ForeCast(item.datetime,item.weather.description);
     })
     res.status(200).json(cleanedData);
+   });
+   
 }
 
 
